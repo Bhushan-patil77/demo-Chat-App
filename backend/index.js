ebcc1044e35a13074f320cb2032bb885373a9ab1
Bhushan-patil77 <145292155+Bhushan-patil77@users.noreply.github.com>
2024-10-14 19:52:39 +0530

    change in cors

--- backend/index.js
+++ backend/index.js
@@ -18,12 +18,13 @@ const server = http.createServer(app)
 // CORS options for Socket.io
 const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:5173', // Allow frontend origin
+      origin: 'http://localhost:5173',
       methods: ['GET', 'POST'],
-      allowedHeaders: ['Content-Type'],
       credentials: true
-    }
+    },
+    transports: ['websocket', 'polling'] // Allow both WebSocket and polling as fallback
   });
+  
 
 io.on('connection', (socket) => {
   console.log('socket connected on ', socket.id);
